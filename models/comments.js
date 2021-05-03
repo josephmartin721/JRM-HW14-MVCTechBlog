d7fb0704d6270020fb1a30eefcd878e38eba0c52
josephmartin721 <josephmartin721@yahoo.com>
2021-05-03 13:09:57 -0500

    index for models/models for main posts and comments

--- models/comments.js
+++ models/comments.js
@@ -36,14 +36,12 @@ Comment.init(
         validate: {
             len: [1]
         }
-      }
-  },
+      }},
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
     modelName: 'comment'
-  }
-);
+  });
 
 module.exports = Comment;
