b3b70205d55a87ee093bd5c3271099fb7cd815eb
josephmartin721 <josephmartin721@yahoo.com>
2021-05-03 23:19:10 -0500

    more bug testing

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const bcrypt = require('bcrypt');
 const path = require('path');
-const mysql2 = require('mysql2');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
-const app = express();
-const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'mistahj',
-    cookie: {maxAge: 7200000},
+    cookie: {maxAge: 8800000},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
     })
 };
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
