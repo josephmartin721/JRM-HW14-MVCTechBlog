7d833cb51ff4dff4c7c957c9cc85cf19222b2c7a
josephmartin721 <josephmartin721@yahoo.com>
2021-05-03 22:18:56 -0500

    cross-referencing handlebars with stylesheet

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,31 +1,30 @@
 async function editFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const content = document.querySelector('input[name="content"]').value.trim();
-    console.log(title);
-    console.log(content);
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const post_content = document.querySelector('input[name="post_content"]').value.trim();
+  console.log(title);
+  console.log(post_content);
 
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
       
-      const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          post_id: id, title, content
-        }),
-        headers: {
-          'Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+      body: JSON.stringify({
+      post_id: id, title, post_content
+      }),
+      headers: {
+        'Type': 'application/json'
       }
-
+    });
+      
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
 }
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
